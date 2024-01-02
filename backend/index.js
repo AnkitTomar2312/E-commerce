59dbe965f981d9ab12be3c9f2bc9156896c0a776
Ankit Tomar <ankitjdec@gmail.com>
2024-01-02 19:56:31 +0530

    update one prodcut api finsihed

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 require("./db/config");
 const User = require("./db/Users");
@@ -51,4 +52,27 @@ app.delete("/product/:id", async (req, res) => {
   const result = await Product.deleteOne({ _id: req.params.id });
   res.send(result);
 });
+
+//To Update Find one Product API:-
+app.get("/product/:id", async (req, res) => {
+  let result = await Product.findOne({ _id: req.params.id });
+  if (result) {
+    res.send(result);
+  } else {
+    res.send("result not found");
+  }
+});
+//updating main Api
+app.put("/product/:id", async (req, res) => {
+  let result = await Product.updateOne(
+    {
+      _id: req.params.id,
+    },
+    {
+      $set: req.body,
+    }
+  );
+  res.send(result);
+});
+
 app.listen(5000);
